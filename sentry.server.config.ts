9420473dd8b2d592836ca1677935e1bee555efb2
Sahana-pixel <sahanaacharya85@gmail.com>
2024-11-18 22:36:22 +0530

    added appointment form

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://db0aedb20effe4fca2f9b7a2eec54a8a@o4508286000955392.ingest.de.sentry.io/4508286004035664",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
